a2ad61ab4b526c1d9ad5866ac8caa6373f55f492
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-09-14 10:51:43 -0500

    feat: add routing and layout for country pages; implement home and by-capital components

--- 04-country-app/src/app/app.routes.ts
+++ 04-country-app/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './contry/pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'country',
+    loadChildren: () => import('./contry/country.routes').then((m) => m.countryRoutes),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
---
+++ 04-country-app/src/app/contry/country.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { CountryLayoutComponent } from './layouts/country-layout/country-layout.component';
+
+export const countryRoutes: Routes = [
+  {
+    path: '',
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital',
+        loadComponent: () => import('./pages/by-capital-page/by-capital-page.component').then((m) => m.ByCapitalPageComponent),
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital',
+      }
+    ]
+  },
+];
