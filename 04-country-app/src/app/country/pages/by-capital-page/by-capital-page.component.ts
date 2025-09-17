6892d69f4b5a77e6be6168d9c479ca8e3fbb597f
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-09-16 23:00:52 -0500

    feat: update by-capital page to use countryResource for error handling and data loading

--- 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from "../../../shared/components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { CountryService } from '../../services/country.service';
 import { Country } from '../../interfaces/country.interface';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -12,29 +13,39 @@ import { Country } from '../../interfaces/country.interface';
 })
 export class ByCapitalPageComponent {
   countryService = inject(CountryService);
-  isLoading = signal(false);
-  isError = signal<string | null>(null);
-  countries = signal<Country[]>([]);
+  capital = signal('');
 
-  onSearchCapital(capital: string) {
-    if (this.isLoading()) return;
-    this.isLoading.set(true);
-    this.isError.set(null);
-    this.countries.set([]);
-    this.countryService.searchByCapital(capital).subscribe({
-      next: (countries) => {
-        this.isLoading.set(false);
-        if (countries.length === 0) {
-          this.isError.set(`No countries found with capital "${capital}"`);
-          return;
-        }
-        this.countries.set(countries);
-      },
-      error: (error) => {
-        this.isLoading.set(false);
-        this.isError.set(error.message ?? 'An error occurred while searching for countries');
-        console.error({ error });
-      }
-    });
-  }
+  countryResource = resource({
+    params: () => ({ capital: this.capital() }),
+    loader: async({ params }) => {
+      if (!params.capital) return [];
+      return firstValueFrom(this.countryService.searchByCapital(params.capital));
+    }
+  })
+
+  // isLoading = signal(false);
+  // isError = signal<string | null>(null);
+  // countries = signal<Country[]>([]);
+
+  // onSearchCapital(capital: string) {
+  //   if (this.isLoading()) return;
+  //   this.isLoading.set(true);
+  //   this.isError.set(null);
+  //   this.countries.set([]);
+  //   this.countryService.searchByCapital(capital).subscribe({
+  //     next: (countries) => {
+  //       this.isLoading.set(false);
+  //       if (countries.length === 0) {
+  //         this.isError.set(`No countries found with capital "${capital}"`);
+  //         return;
+  //       }
+  //       this.countries.set(countries);
+  //     },
+  //     error: (error) => {
+  //       this.isLoading.set(false);
+  //       this.isError.set(error.message ?? 'An error occurred while searching for countries');
+  //       console.error({ error });
+  //     }
+  //   });
+  // }
 }
