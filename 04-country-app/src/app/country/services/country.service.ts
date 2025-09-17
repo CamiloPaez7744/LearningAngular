a2315d1491d23630c6ef294c41b21d61ac628156
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-09-16 22:45:10 -0500

    feat: add error handling to country service and update by-capital page for error display

--- 04-country-app/src/app/country/services/country.service.ts
+++ 04-country-app/src/app/country/services/country.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/res-countries.interface';
 import { Country } from '../interfaces/country.interface';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
 import { CountryMapper } from '../mappers/country.mapper';
 
 const API_URL = 'https://restcountries.com/v3.1';
@@ -17,7 +17,11 @@ export class CountryService {
     capital = capital.toLowerCase().trim();
 
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${capital}`).pipe(
-      map( restCountry => CountryMapper.toCountries(restCountry))
+      map(restCountry => CountryMapper.toCountries(restCountry)),
+      catchError((error) => {
+        console.error({ error });
+        return throwError(() => new Error(`Error fetching countries with capital "${capital}"`));
+      })
     );
   }
 }
