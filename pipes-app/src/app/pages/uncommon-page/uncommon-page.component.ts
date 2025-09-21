37c521f73cb4c6ff86404b893472dcbc917a0ec7
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-09-21 18:56:12 -0500

    feat: update uncommon page with i18nSelect pipe and client switching functionality

--- pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Bruce',
+  gender: 'Male',
+  age: 35,
+  address: 'Ottawa, Canada'
+}
+
+const client2 = {
+  name: 'Diana',
+  gender: 'Female',
+  age: 25,
+  address: 'New York, USA'
+}
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export class UncommonPageComponent { }
+export class UncommonPageComponent {
+  // i18nSelect
+  client = signal(client1);
+  changeClient(): void {
+    this.client.set(this.client() === client1 ? client2 : client1);
+  }
+
+  invitationMap = {
+    'Male': 'Sr.',
+    'Female': 'Sra.',
+  }
+ }
