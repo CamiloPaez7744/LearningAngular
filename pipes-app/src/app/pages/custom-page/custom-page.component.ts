40533a3f14c78bc5b20aaabc7299c8709bac3a39
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-09-23 10:11:04 -0500

    feat: add heroMapColor and heroCreator pipes for enhanced hero data display in custom page

--- pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -3,10 +3,13 @@ import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/hero.data';
 import { canFlyCasePipe } from '../../pipes/can-fly-case.pipe';
 import { HeroColorCasePipe } from '../../pipes/hero-color-case.pipe';
+import { HeroMapColorPipe } from '../../pipes/hero-map-color.pipe';
+import { TitleCasePipe } from '@angular/common';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, canFlyCasePipe, HeroColorCasePipe],
+  imports: [ToggleCasePipe, canFlyCasePipe, HeroColorCasePipe, HeroMapColorPipe, TitleCasePipe, HeroCreatorPipe],
   templateUrl: './custom-page.component.html',
 })
 export class CustomPageComponent {
