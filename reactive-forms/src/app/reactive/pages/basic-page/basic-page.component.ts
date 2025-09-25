c43bce01668703767fce4ae1d4d1394eb0eb7b99
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-09-24 21:08:07 -0500

    feat: implement reactive form with validation for product details

--- reactive-forms/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ reactive-forms/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,24 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent { }
+export class BasicPageComponent {
+  private fb = inject(FormBuilder);
+
+  myForm = this.fb.group({
+    name: ['',/** Validators synchronous */ [Validators.required, Validators.minLength(3)], /** Validators asynchronous */ []],
+    price: [0, [Validators.min(10), Validators.required]],
+    inStorage: [0, [Validators.min(0), Validators.required]],
+  });
+
+  // myForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+}
