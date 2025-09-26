88bde23cbd387628c1e3ebf0fe7f2fd75a6a4ff8
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-09-25 21:58:28 -0500

    feat: enhance dynamic form with validation and error handling for fields

--- reactive-forms/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ reactive-forms/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,48 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtil } from '../../../utils/form.util';
 
 @Component({
   selector: 'app-dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.component.html',
 })
-export class DynamicPageComponent { }
+export class DynamicPageComponent {
+  private fb = inject(FormBuilder);
+  formUtil = FormUtil;
+
+  dynamicForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ], [  Validators.required, Validators.minLength(1)]),
+  });
+
+  newFavoriteGame = new FormControl('', Validators.required);
+  // newFavoriteGame = this.fb.control('', Validators.required);
+
+  addFavoriteGame() {
+    if (this.newFavoriteGame.invalid) return;
+    const newGame = this.newFavoriteGame.value;
+
+    this.favoriteGames.push(this.fb.control(newGame, Validators.required));
+    this.newFavoriteGame.reset();
+  }
+
+  deleteFavoriteGame(index: number) {
+    this.favoriteGames.removeAt(index);
+  }
+
+  get favoriteGames() {
+    return this.dynamicForm.get('favoriteGames') as FormArray;
+  }
+
+  onSubmit() {
+    if (this.dynamicForm.invalid) {
+      this.dynamicForm.markAllAsTouched();
+      return;
+    }
+  }
+}
--- reactive-forms/src/app/utils/form.util.ts
+++ reactive-forms/src/app/utils/form.util.ts
@@ -1,25 +1,39 @@
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 
 export class FormUtil {
   static isValidField(form: FormGroup, field: string): boolean | null {
     return form.controls[field].errors && form.controls[field].touched;
   }
 
-  static getFieldError(form: FormGroup, field: string): string | null {
-    if (!form.controls[field]) return null;
+  static isValidFieldInArray(FormArray: FormArray, index: number): boolean | null {
+    return FormArray.at(index).errors && FormArray.at(index).touched;
+  }
 
-    const errors = form.controls[field].errors || {};
-    for (const key of Object.keys(errors)) {
+  static getTextError(error: ValidationErrors): string | null {
+    for (const key of Object.keys(error)) {
       switch (key) {
         case 'required':
           return 'This field is required.';
         case 'minlength':
-          return `Minimum ${errors['minlength'].requiredLength} characters.`;
+          return `Minimum ${error['minlength'].requiredLength} characters.`;
         case 'min':
-          return `The minimum value is ${errors['min'].min}.`;
+          return `The minimum value is ${error['min'].min}.`;
       }
-    }
 
+    }
     return null;
+  }
+
+  static getFieldError(form: FormGroup, field: string): string | null {
+    if (!form.controls[field]) return null;
+
+    const errors = form.controls[field].errors || {};
+    return FormUtil.getTextError(errors);
+  }
+
+  static getFieldErrorInArray(FormArray: FormArray, index: number): string | null {
+    if (!FormArray.at(index)) return null;
+    const errors = FormArray.at(index).errors || {};
+    return FormUtil.getTextError(errors);
   }
 }
