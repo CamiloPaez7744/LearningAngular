ff1bfc39408826d7a4ee0994f00af7a182df37f3
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-09-26 19:35:39 -0500

    feat: implement reactive form with validation for switches page

--- reactive-forms/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ reactive-forms/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,28 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtil } from '../../../utils/form.util';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.component.html',
 })
-export class SwitchesPageComponent { }
+export class SwitchesPageComponent {
+  public fb = inject(FormBuilder);
+  formUtil = FormUtil;
+
+  myForm: FormGroup = this.fb.group({
+    gender: [, [Validators.required]],
+    wantNotification: [true],
+    termsAndConditions: [false, [Validators.requiredTrue]],
+  });
+
+  onSubmit() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+  }
+
+}
