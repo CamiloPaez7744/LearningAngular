48ea6c5ccc28b0a938b3d23a3b951e66c3cb025f
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-09-27 20:57:53 -0500

    feat: enhance country selection form with reactive updates and reset logic on region change

--- reactive-forms/src/app/country/pages/country-page/country-page.component.ts
+++ reactive-forms/src/app/country/pages/country-page/country-page.component.ts
@@ -1,8 +1,9 @@
 import { JsonPipe } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CountryService } from '../../services/country.service';
 import { Country } from '../../interfaces/country.interface';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -18,8 +19,38 @@ export class CountryPageComponent {
   bordersByCountry = signal<Country[]>([]);
 
   myForm = this.fb.group({
-    region:   ['', [Validators.required]],
-    country:  ['', [Validators.required]],
-    border:   ['', [Validators.required]]
+    region: ['', [Validators.required]],
+    country: ['', [Validators.required]],
+    border: ['', [Validators.required]]
   });
+
+  onFormChanges = effect((onCleanup) => {
+    const regionSubscribe = this.onRegionChange();
+    onCleanup(() => {
+      regionSubscribe.unsubscribe();
+    });
+  });
+
+  onRegionChange() {
+    return this.myForm.get('region')!?.valueChanges
+      .pipe(
+        tap(() => { this.myForm.get('country')!?.reset(''); }),
+        tap(() => { this.myForm.get('border')!?.reset(''); }),
+        tap(() => {
+          this.countriesByRegion.set([]);
+          this.bordersByCountry.set([]);
+        }),
+        switchMap((region) => this.countryService.getCountriesByRegion(region!)),
+      )
+      .subscribe(
+        (countries) => {
+          console.log({ countries });
+          this.countriesByRegion.set(countries);
+        }
+      );
+  }
+
+  // formRegionChanges = this.myForm.get('region')?.valueChanges.subscribe(
+  //   console.log()
+  // );
 }
