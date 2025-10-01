a9d4d02eac3c34040647adf879da0256b8ab7e21
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-09-30 20:05:32 -0500

    feat: add zoom controls to full-screen map page and implement zoom functionality

--- 08-maps-app/src/app/pages/full-screen-map-page/full-screen-map-page.component.ts
+++ 08-maps-app/src/app/pages/full-screen-map-page/full-screen-map-page.component.ts
@@ -1,12 +1,13 @@
 
-import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+import { AfterViewInit, Component, effect, ElementRef, signal, viewChild } from '@angular/core';
 import mapboxgl from 'mapbox-gl';
 import { environment } from '../../../environments/environment';
+import { DecimalPipe } from '@angular/common';
 
 mapboxgl.accessToken = environment.mapboxKey;
 @Component({
   selector: 'app-full-screen-map-page',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './full-screen-map-page.component.html',
   styles: `
   div {
@@ -14,10 +15,30 @@ mapboxgl.accessToken = environment.mapboxKey;
     height: calc(100vh - 64px);
     background-color: lightgray;
   }
+
+  #controls {
+    background-color: white;
+    padding: 10px;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    position: fixed;
+    bottom: 50px;
+    right: 50px;
+    z-index: 999;
+    width: 250px;
+  }
   `
 })
 export class FullScreenMapPageComponent implements AfterViewInit {
   divElement = viewChild<ElementRef>('map');
+  map = signal<mapboxgl.Map | null>(null);
+
+  zoom = signal(5);
+
+  zoomEffect = effect(() => {
+    if (!this.map()) return;
+    this.map()?.setZoom(this.zoom());
+  });
 
   ngAfterViewInit(): void {
     if (!this.divElement()) return;
@@ -26,8 +47,18 @@ export class FullScreenMapPageComponent implements AfterViewInit {
       container: element?.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11', // style URL
       center: [-74.0721, 4.7110], // starting position [lng, lat]
-      zoom: 9 // starting zoom
+      zoom: this.zoom() // starting zoom
     });
+    this.mapListeners(map);
+  }
+
+  mapListeners(map: mapboxgl.Map) {
+    map.on('zoomend', (event) => {
+      const newZoom = event.target.getZoom();
+      this.zoom.set(newZoom);
+    });
+
+    this.map.set(map);
   }
 
 }
