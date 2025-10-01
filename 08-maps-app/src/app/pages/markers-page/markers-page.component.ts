6abc11111aad7376a23ce977a46d8c252f72678b
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-10-01 07:13:38 -0500

    feat: implement markers page with Mapbox integration and map controls

--- 08-maps-app/src/app/pages/markers-page/markers-page.component.ts
+++ 08-maps-app/src/app/pages/markers-page/markers-page.component.ts
@@ -1,8 +1,67 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, effect, ElementRef, signal, viewChild } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
+import { environment } from '../../../environments/environment';
 
+mapboxgl.accessToken = environment.mapboxKey;
 @Component({
   selector: 'app-markers-page',
   imports: [],
   templateUrl: './markers-page.component.html',
 })
-export class MarkersPageComponent { }
+export class MarkersPageComponent implements AfterViewInit {
+  divElement = viewChild<ElementRef>('map');
+  map = signal<mapboxgl.Map | null>(null);
+
+  zoom = signal(5);
+  coordinates = signal({
+    lng: -74.0721,
+    lat: 4.7110
+  });
+
+  zoomEffect = effect(() => {
+    if (!this.map()) return;
+    this.map()?.setZoom(this.zoom());
+  });
+
+  ngAfterViewInit(): void {
+    if (!this.divElement()) return;
+    const element = this.divElement();
+    const { lng, lat } = this.coordinates();
+    const map = new mapboxgl.Map({
+      container: element?.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: [lng, lat], // starting position [lng, lat]
+      zoom: this.zoom() // starting zoom
+    });
+
+    const marker = new mapboxgl.Marker()
+      .setLngLat([lng, lat])
+      .addTo(map);
+    this.mapListeners(map);
+  }
+
+  mapListeners(map: mapboxgl.Map) {
+    map.on('zoomend', (event) => {
+      const newZoom = event.target.getZoom();
+      this.zoom.set(newZoom);
+    });
+
+    map.on('move', () => {
+      const { lng, lat } = map.getCenter();
+      this.coordinates.set({ lng, lat });
+    });
+
+    map.addControl(new mapboxgl.FullscreenControl());
+    map.addControl(new mapboxgl.NavigationControl());
+    map.addControl(new mapboxgl.ScaleControl());
+    map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true,
+      showUserHeading: true
+    }));
+
+    this.map.set(map);
+  }
+}
