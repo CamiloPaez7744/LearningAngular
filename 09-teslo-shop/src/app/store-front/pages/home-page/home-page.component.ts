ee93e74b44d3b3283d3e3ffd151c13defce43e70
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-10-09 20:51:14 -0500

    feat: integrate current page tracking in pagination component for home page

--- 09-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
+++ 09-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { Product, ProductsResponse } from '@products/interfaces/product-response.interface';
 import { ProductsService } from '@products/services/products.service';
-import { of, tap } from 'rxjs';
+import { map, of, tap } from 'rxjs';
 import { PaginationComponent } from "@shared/components/pagination/pagination.component";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -15,10 +16,23 @@ export class HomePageComponent {
   private productsService = inject(ProductsService);
   product = signal<Product[] | null>(null);
 
+  activatedRoute = inject(ActivatedRoute);
+  currentPage = toSignal(this.activatedRoute.queryParamMap.pipe(
+    map(params => params.get('page') ? +params.get('page')! : 1),
+    map(page => (isNaN(page) || page < 1 ? 1 : page)),
+  ),
+    {
+      initialValue: 1,
+    }
+  );
+
   productsResource = rxResource({
-    params: () => ({  }),
+    params: () => ({ page: this.currentPage() }),
     stream: ({ params }) => {
-      return this.productsService.getAllProducts({}).pipe(
+      return this.productsService.getAllProducts({
+        offset: (params.page - 1) * 10,
+        limit: 10,
+      }).pipe(
         tap((response) => {
           this.product.set(response.products);
         })
