9be81255a9f19d144a8730c6cab4bf97612cc8d0
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-10-11 21:36:16 -0500

    feat: enhance login page with reactive form and error handling

--- 09-teslo-shop/src/app/auth/pages/login-page/login-page.component.ts
+++ 09-teslo-shop/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
 })
-export class LoginPageComponent { }
+export class LoginPageComponent {
+  fb = inject(FormBuilder);
+  hasError = signal(false);
+  isPosting = signal(false);
+
+  loginForm = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      this.hasError.set(true);
+      setTimeout(() => this.hasError.set(false), 3000);
+      return;
+    }
+  }
+}
