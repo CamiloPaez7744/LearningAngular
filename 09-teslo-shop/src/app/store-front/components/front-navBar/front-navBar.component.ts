9c5b03253ce63f9c9c196358ec4a1e88bca3536b
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-10-16 20:17:15 -0500

    feat: enhance front navigation bar with authentication status display and loading state

--- 09-teslo-shop/src/app/store-front/components/front-navBar/front-navBar.component.ts
+++ 09-teslo-shop/src/app/store-front/components/front-navBar/front-navBar.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'front-nav-bar',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './front-navBar.component.html',
 })
-export class FrontNavBarComponent { }
+export class FrontNavBarComponent {
+  authService = inject(AuthService);
+
+}
