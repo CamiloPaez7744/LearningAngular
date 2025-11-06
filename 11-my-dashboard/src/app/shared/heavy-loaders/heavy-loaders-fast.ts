177ca8bde9f7dd1dcbd304024841b38891095612
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-11-05 22:30:24 -0500

    feat: add initial dashboard components and pages with basic templates

---
+++ 11-my-dashboard/src/app/shared/heavy-loaders/heavy-loaders-fast.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loaders-fast',
+  imports: [],
+  template: `
+  <div>Fast loader</div>
+  `,
+})
+export class HeavyLoadersFast { }
---
+++ 11-my-dashboard/src/app/shared/heavy-loaders/heavy-loaders-slow.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loaders-slow',
+  imports: [],
+  template: `
+  <div>Slow loader</div>
+  `,
+})
+export class HeavyLoadersSlow { }
