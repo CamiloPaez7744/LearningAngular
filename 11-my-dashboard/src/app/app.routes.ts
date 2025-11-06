09b26f9a24afa2e5cf6f1e56974f180321bd8a18
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-11-05 22:42:46 -0500

    feat: add routing for dashboard and its child components, update dashboard template to include router outlet

--- 11-my-dashboard/src/app/app.routes.ts
+++ 11-my-dashboard/src/app/app.routes.ts
@@ -1,3 +1,55 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./dashboard/dashboard').then(m => m.Dashboard),
+        children: [
+            {
+                path: 'change-detection',
+                title: 'Change Detection',
+                loadComponent: () => import('./dashboard/pages/change-detection/change-detection').then(m => m.ChangeDetection)
+            },
+            {
+                path: 'control-flow',
+                title: 'Control Flow',
+                loadComponent: () => import('./dashboard/pages/control-flow/control-flow').then(m => m.ControlFlow)
+            },
+            {
+                path: 'defer-options',
+                title: 'Defer Options',
+                loadComponent: () => import('./dashboard/pages/defer-options/defer-options').then(m => m.DeferOptions)
+            },
+            {
+                path: 'defer-views',
+                title: 'Defer Views',
+                loadComponent: () => import('./dashboard/pages/defer-views/defer-views').then(m => m.DeferViews)
+            },
+            {
+                path: 'user/:id',
+                title: 'User',
+                loadComponent: () => import('./dashboard/pages/user/user').then(m => m.User)
+            },
+            {
+                path: 'user-list',
+                title: 'User List',
+                loadComponent: () => import('./dashboard/pages/users/users').then(m => m.Users)
+            },
+            {
+                path: 'view-transition',
+                title: 'View Transition',
+                loadComponent: () => import('./dashboard/pages/view-transition/view-transition').then(m => m.ViewTransition)
+            },
+            {
+                path: '',
+                redirectTo: 'change-detection',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    }
+];
