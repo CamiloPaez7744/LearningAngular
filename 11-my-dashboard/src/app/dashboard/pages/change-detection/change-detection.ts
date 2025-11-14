8d60200dae05298be4f16f00ed481cb14ae4f2e6
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-11-13 22:53:11 -0500

    feat: implement change detection examples with initial framework signals and attributes

--- 11-my-dashboard/src/app/dashboard/pages/change-detection/change-detection.ts
+++ 11-my-dashboard/src/app/dashboard/pages/change-detection/change-detection.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { Title } from "@shared/title/title";
 
 @Component({
   selector: 'app-change-detection',
-  imports: [],
+  imports: [Title, JsonPipe],
   templateUrl: './change-detection.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChangeDetection { }
+export class ChangeDetection {
+  public frameworkAsSignal = signal({ name: 'Angular', popular: true });
+
+  public frameworkAsAttribute = { name: 'React', popular: true };
+
+  constructor() {
+    setTimeout(() => {
+      this.frameworkAsSignal.set({ name: 'Vue', popular: true });
+      // this.frameworkAsAttribute.name = 'Vue';
+    }, 3000);
+  }
+}
