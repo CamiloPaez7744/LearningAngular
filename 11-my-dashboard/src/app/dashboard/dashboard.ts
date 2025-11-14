6e85d8922f7e302b7565fcba2d1215a37fc64b26
Camilo Páez <camiloapaez@ucundinamarca.edu.co>
2025-11-13 21:27:21 -0500

    feat: implement side menu and enhance dashboard layout with responsive design

--- 11-my-dashboard/src/app/dashboard/dashboard.ts
+++ 11-my-dashboard/src/app/dashboard/dashboard.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenu } from "../shared/side-menu/side-menu";
+import { routes } from '../app.routes';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenu],
   templateUrl: './dashboard.html',
 })
-export class Dashboard { }
+export class Dashboard {
+  public menuItems = routes.map(route => route.children ?? []).flat()
+    .filter(r => r.path && r.path !== '')
+    .filter(r => !(r.path?.includes(':')));
+
+  constructor() {
+
+  }
+}
